24d76aa7ae1e335f7fd455b27785341f76f3dad8
Georgi Sariev <73587992+gssariev@users.noreply.github.com>
2024-07-14 22:12:10 +0200

    Update Handle-SubtitlesIssue.ps1

--- functions/Handle-SubtitlesIssue.ps1
+++ functions/Handle-SubtitlesIssue.ps1
@@ -1,24 +1,3 @@
-function Extract-Offset {
-    param (
-        [string]$message
-    )
-    if ($message -match "offset\s+(\d+)") {
-        $offsetValue = [int]$matches[1]
-        if ($offsetValue -ge 0 -and $offsetValue -le 60) {
-            return 60
-        } elseif ($offsetValue -ge 61 -and $offsetValue -le 120) {
-            return 120
-        } elseif ($offsetValue -ge 121 -and $offsetValue -le 300) {
-            return 300
-	    } elseif ($offsetValue -ge 301 -and $offsetValue -le 600) {
-            return 600
-        } else {
-            return $offsetValue
-        }
-    }
-    return $null
-}
-
 function Handle-SubtitlesIssue {
     param ([psobject]$payload)
 
