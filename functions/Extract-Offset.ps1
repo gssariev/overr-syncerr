4302221ebd81a6266a1780671059d15f099d7318
Georgi Sariev <73587992+gssariev@users.noreply.github.com>
2024-07-14 22:12:21 +0200

    Update Extract-Offset.ps1

--- functions/Extract-Offset.ps1
+++ functions/Extract-Offset.ps1
@@ -3,7 +3,18 @@ function Extract-Offset {
         [string]$message
     )
     if ($message -match "offset\s+(\d+)") {
-        return [int]$matches[1]
+        $offsetValue = [int]$matches[1]
+        if ($offsetValue -ge 0 -and $offsetValue -le 60) {
+            return 60
+        } elseif ($offsetValue -ge 61 -and $offsetValue -le 120) {
+            return 120
+        } elseif ($offsetValue -ge 121 -and $offsetValue -le 300) {
+            return 300
+	    } elseif ($offsetValue -ge 301 -and $offsetValue -le 600) {
+            return 600
+        } else {
+            return $offsetValue
+        }
     }
     return $null
 }
