f97b7ba0cce390bd2a8072b236bb15af25f1042d
Austin Kirsch <37373320+kirsch33@users.noreply.github.com>
2024-08-11 10:12:28 -0400

    Update Contains-SyncKeyword.ps1
    always return true if blank sync keywords array

--- functions/Contains-SyncKeyword.ps1
+++ functions/Contains-SyncKeyword.ps1
@@ -1,10 +1,15 @@
 function Contains-SyncKeyword {
     param ([string]$issueMessage, [array]$syncKeywords)
-    $lowercaseMessage = $issueMessage.ToLower()
-    foreach ($keyword in $syncKeywords) {
-        if ($lowercaseMessage.Contains($keyword)) {
-            return $true
-        }
+    if ($syncKeywords.Length -eq 0) {
+        return $true
     }
-    return $false
+    else {
+        $lowercaseMessage = $issueMessage.ToLower()
+        foreach ($keyword in $syncKeywords) {
+            if ($lowercaseMessage.Contains($keyword)) {
+                return $true
+            }
+        }
+        return $false
+    }
 }
