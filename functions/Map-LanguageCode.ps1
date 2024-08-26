bfb3cd2c52930b4823347b04034d697d697b3a52
Georgi Sariev <73587992+gssariev@users.noreply.github.com>
2024-08-26 11:16:33 +0200

    Update Map-LanguageCode.ps1

--- functions/Map-LanguageCode.ps1
+++ functions/Map-LanguageCode.ps1
@@ -1,14 +1,4 @@
 function Map-LanguageCode {
     param ([string]$languageCode, [hashtable]$languageMap)
-    if (-not $languageCode) {
-        if ($languageMap.count -eq 1) {
-            return $($languageMap.Values)
-        }
-        else {
-            return $($languageMap.Values)[0]
-        }
-    }
-    else {
-        return $languageMap[$languageCode]
-    }
+    return $languageMap[$languageCode]
 }
