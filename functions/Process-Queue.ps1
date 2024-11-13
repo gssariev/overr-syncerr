c4956d744ae6f3b472981693d092ae1f6942c543
Georgi Sariev <73587992+gssariev@users.noreply.github.com>
2024-11-13 09:25:06 +0100

    Delete functions/Process-Queue.ps1

--- functions/Process-Queue.ps1
+++
@@ -1,9 +0,0 @@
-function Process-Queue {
-    while ($queue.Count -gt 0) {
-        $jsonPayload = $queue.Dequeue()
-        Write-Output "Processing payload: $jsonPayload"
-        Handle-Webhook -jsonPayload $jsonPayload
-        Start-Sleep -Seconds 5
-    }
-    Write-Output "All payloads processed."
-}
