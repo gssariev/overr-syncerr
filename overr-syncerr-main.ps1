c53d57393e1a534228ce890184ca7da04973cf7c
Georgi Sariev <73587992+gssariev@users.noreply.github.com>
2025-03-01 23:10:11 +0100

    Adding new variables
    Kometa, GPT

--- overr-syncerr-main.ps1
+++ overr-syncerr-main.ps1
@@ -1,7 +1,16 @@
 # Import Functions
 Get-ChildItem -Path './functions/*.ps1' | ForEach-Object { . $_.FullName }
 
+# Parse and set up environment variables
 $requestIntervalCheck = $env:CHECK_REQUEST_INTERVAL
+$maintainerrUrl = $env:MAINTAINERR_URL
+$audioTrackFilter = $env:AUDIO_TRACK_FILTER
+$maintainerrApiKey = $env:MAINTAINERR_API_KEY
+$unwatchedLimit = [int]$env:UNWATCHED_LIMIT
+$movieQuotaLimit = [int]$env:MOVIE_QUOTA_LIMIT
+$movieQuotaDays = [int]$env:MOVIE_QUOTA_DAYS
+$tvQuotaLimit = [int]$env:TV_QUOTA_LIMIT
+$tvQuotaDays = [int]$env:TV_QUOTA_DAYS
 $bazarrApiKey = $env:BAZARR_API_KEY
 $bazarrUrl = $env:BAZARR_URL
 $radarrApiKey = $env:RADARR_API_KEY
@@ -16,6 +25,8 @@ $sonarr4kApiKey = $env:SONARR_4K_API_KEY
 $sonarr4kUrl = $env:SONARR_4K_URL
 $overseerrApiKey = $env:OVERSEERR_API_KEY
 $overseerrUrl = $env:OVERSEERR_URL
+$plexClientId = $env:SERVER_CLIENTID
+$kometaConfig = $env:KOMETA_CONFIG_PATH
 $plexToken = $env:PLEX_TOKEN
 $plexHost = $env:PLEX_HOST
 $animeLibraryName = $env:ANIME_LIBRARY_NAME
@@ -24,10 +35,22 @@ $seriesLibraryName = $env:SERIES_LIBRARY_NAME
 $port = $env:PORT
 $enableMediaAvailableHandling = $env:ENABLE_MEDIA_AVAILABLE_HANDLING -eq "true"
 $queue = [System.Collections.Queue]::new()
+$openAiApiKey = $env:OPEN_AI_API_KEY
+$useGPT = $env:ENABLE_GPT -eq "true"
+$modelGPT = $env:MODEL_GPT
+$maxTokens = [int]$env:MAX_TOKENS
 
 $addLabelKeywords = $env:ADD_LABEL_KEYWORDS
 $languageMapJson = $env:LANGUAGE_MAP
 $syncKeywordsJson = $env:SYNC_KEYWORDS
+
+# Call function at script startup
+Log-Message -Type "INF" -Message "Fetching Plex user tokens..."
+Get-PlexUserTokens -plexToken $plexToken -plexClientId $plexClientId
+
+# Call function at script startup to ensure the file exists
+Log-Message -Type "INF" -Message "Generating user audio preferences..."
+Generate-UserAudioPreferences
 
 try {
     $languageMapPSObject = ConvertFrom-Json -InputObject $languageMapJson
@@ -35,18 +58,18 @@ try {
     $languageMapPSObject.psobject.Properties | ForEach-Object {
         $languageMap.Add($_.Name, $_.Value)
     }
-    Write-Host "Language map parsed successfully"
+    Log-Message -Type "SUC" -Message "Language map parsed successfully"
 } catch {
     $languageMap = @{}
-    Write-Host "Error parsing language map: $_"
+    Log-Message -Type "ERR" -Message "Error parsing language map: $_"
 }
 
 try {
     $syncKeywords = ConvertFrom-Json -InputObject $syncKeywordsJson
-    Write-Host "Sync keywords parsed successfully"
+    Log-Message -Type "SUC" -Message "Sync keywords parsed successfully"
 } catch {
     $syncKeywords = @('sync', 'out of sync', 'synchronize', 'synchronization')
-    Write-Host "Error parsing sync keywords: $_"
+    Log-Message -Type "ERR" -Message "Error parsing sync keywords: $_"
 }
 
 # Fetch Plex Library IDs
@@ -57,18 +80,31 @@ $animeSectionId = $libraryIds[$animeLibraryName]
 $moviesSectionId = $libraryIds[$moviesLibraryName]
 $seriesSectionId = $libraryIds[$seriesLibraryName]
 
-Write-Host "Anime Section ID: $animeSectionId"
-Write-Host "Movies Section ID: $moviesSectionId"
-Write-Host "Series Section ID: $seriesSectionId"
-
+Log-Message -Type "SUC" -Message "Anime Section ID: $animeSectionId"
+Log-Message -Type "SUC" -Message "Movies Section ID: $moviesSectionId"
+Log-Message -Type "SUC" -Message "Series Section ID: $seriesSectionId"
 
 # Check if the environment variable MONITOR_REQUESTS is set to true
 $monitorRequests = $env:MONITOR_REQUESTS -eq "true"
+$collectionsInterval = [int]$env:COLLECTIONS_INTERVAL
 
 if ($monitorRequests) {
-    Start-OverseerrRequestMonitor -overseerrUrl $overseerrUrl -plexHost $plexHost -plexToken $plexToken -overseerrApiKey $overseerrApiKey -seriesSectionId $seriesSectionId -animeSectionId $animeSectionId -requestIntervalCheck $requestIntervalCheck
+    Start-OverseerrRequestMonitor -overseerrUrl $overseerrUrl `
+                                  -plexHost $plexHost `
+                                  -plexToken $plexToken `
+                                  -overseerrApiKey $overseerrApiKey `
+                                  -seriesSectionId $seriesSectionId `
+                                  -animeSectionId $animeSectionId `
+                                  -requestIntervalCheck $requestIntervalCheck
+    Log-Message -Type "INF" -Message "Started Overseerr request monitor."
 } else {
-    Write-Host "Request monitoring is not enabled. Skipping Overseerr request monitor."
+    Log-Message -Type "WRN" -Message "Request monitoring is not enabled. Skipping Overseerr request monitor."
+}
+
+if ($useGPT) {
+    Log-Message -Type "INF" -Message "GPT is used for subtitle translation."
+} else {
+    Log-Message -Type "INF" -Message "Bazarr (Google API) is used for subtitle translation."
 }
 
 # HTTP listener
@@ -76,7 +112,7 @@ $listener = [System.Net.HttpListener]::new()
 $listener.Prefixes.Add("http://*:$port/")
 $listener.Start()
 
-Write-Host "Listening for webhooks on http://localhost:$port/"
+Log-Message -Type "INF" -Message "Listening for webhooks on http://localhost:$port/"
 
 while ($true) {
     $context = $listener.GetContext()
@@ -88,7 +124,7 @@ while ($true) {
         $jsonPayload = $reader.ReadToEnd()
         $reader.Close()
 
-        Write-Host "Received payload: $jsonPayload"
+        Log-Message -Type "INF" -Message "Received payload: $jsonPayload"
 
         Enqueue-Payload -Payload $jsonPayload
         Process-Queue
