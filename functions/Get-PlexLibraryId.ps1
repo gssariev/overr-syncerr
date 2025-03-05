599406c1a74646738ed2beac685ffce417fe409a
Georgi Sariev <73587992+gssariev@users.noreply.github.com>
2025-03-05 20:39:35 +0100

    Fixed null username, duplicate libraries, multi library support

--- functions/Get-PlexLibraryId.ps1
+++ functions/Get-PlexLibraryId.ps1
@@ -2,31 +2,56 @@ function Get-PlexLibraryIds {
     param (
         [string]$plexHost,
         [string]$plexToken,
-        [string[]]$libraryNames
+        [pscustomobject]$libraryCategories
     )
 
-    $url = "$plexHost/library/sections/all?X-Plex-Token=$plexToken"
- 
+    $url = "$plexHost/library/sections/all"+"?X-Plex-Token=$plexToken"
+
     try {
         $response = Invoke-RestMethod -Uri $url -Method Get -ContentType "application/xml"
         $libraries = $response.MediaContainer.Directory
 
         $libraryIds = @{}
 
-        foreach ($libraryName in $libraryNames) {
-            $library = $libraries | Where-Object { $_.title -ieq $libraryName }
-            if ($null -ne $library) {
-                $libraryIds[$libraryName] = $library.key
-       
-            } else {
-                Write-Host "Library '$libraryName' not found."
-                $libraryIds[$libraryName] = $null
+        foreach ($category in $libraryCategories.Keys) {
+            $filteredLibraryNames = $libraryCategories[$category] | Where-Object { $_ -and $_.Trim() -ne "" }
+
+            if ($filteredLibraryNames.Count -lt $libraryCategories[$category].Count) {
+                Log-Message -Type "WRN" -Message "One or more library names were null or empty in category '$category' and have been ignored."
+            }
+
+            # Define correct Plex library types based on category
+            $categoryType = switch ($category) {
+                "Movies" { "movie" }
+                "TV" { "show" }
+                "Anime" { "show" } # Anime is categorized as TV in Plex
+                Default { "" }
+            }
+
+            foreach ($libraryName in $filteredLibraryNames) {
+                # Get all matching libraries with the correct type
+                $matchingLibraries = $libraries | Where-Object { $_.title -ieq $libraryName -and $_.type -eq $categoryType }
+                
+                if ($matchingLibraries) {
+                    # Ensure $libraryIds[$category] is initialized as an array before adding IDs
+                    if (-not $libraryIds.ContainsKey($category)) {
+                        $libraryIds[$category] = @()
+                    }
+                    $libraryIds[$category] += $matchingLibraries.key 
+
+                    Log-Message -Type "SUC" -Message "Library '$libraryName' found in category '$category' with IDs: $($matchingLibraries.key -join ', ')"
+                } else {
+                    Log-Message -Type "ERR" -Message "Library '$libraryName' of type '$category' not found."
+                    if (-not $libraryIds.ContainsKey($category)) {
+                        $libraryIds[$category] = @()
+                    }
+                }
             }
         }
 
         return $libraryIds
     } catch {
-        Write-Host "Error fetching Plex library sections: $_"
+        Log-Message -Type "ERR" -Message "Error fetching Plex library sections: $_"
         return $null
     }
 }
